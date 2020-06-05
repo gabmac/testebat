b365b2725923d4a02a721c11fe1ffa5034824450
gabmac <30659678+gabmac@users.noreply.github.com>
2020-06-05 00:53:07 -0300

    Update teste.bat

--- teste.bat
+++ teste.bat
@@ -8,11 +8,8 @@ for /F "tokens=1-4 delims=: " %%i in ("%time%") do (set /a H=%%i%%j 2>NUL)
 
 for /F "skip=1" %%I in ('wmic path win32_localtime get dayofweek') do (set /a dow=%%I 2>NUL)
 
-echo O horario e %H%
-echo O dia da semana e %dow%
-
-set /a H=830 2>NUL
-set /a dow=5 2>NUL
+set /a H=600 2>NUL
+set /a dow=6 2>NUL
 set call=0
 echo o valor de CAll inicializado %call%
 
@@ -21,12 +18,21 @@ echo O dia da semana e %dow%L
 
 
 
-if 800 Leq %H% if %H% Leq 1600 (set /a call=1)
-if 1600 Leq %H% if %H% Leq 2000 if %dow% Equ 7 (set /a call=1)
-if 2000 Leq %H% if %dow% (set /a call=2)
-if 1600 Leq %H% if %dow% NEQ 7 (set /a call=2)
-if 800 Geq %H% if %dow% (set /a call=2)
+if 800 Leq %H% if %H% Leq 1600 goto T1
+if 1600 Leq %H% if %H% Leq 2000 if %dow% Equ 7 goto T1
+if 2000 Leq %H% goto T23
+if 1600 Leq %H% if %dow% NEQ 7 goto T23
+if 800 Geq %H% goto T23
+
+exit 1
+
+:T1
+    echo "Baixa"
+    goto end
+:T23
+    echo "Baixa Media"
+    goto end
 
 
-echo %call%
+:end
 
